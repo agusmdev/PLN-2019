c4b1d5cbcc22b694fe0d97eab0ba123bb372aef5
gosen <agusmdev@gmail.com>
2019-05-30 01:44:16 -0300

    Added tests for classifier

--- tagging/tests/test_classifier.py
+++ tagging/tests/test_classifier.py
@@ -14,11 +14,63 @@ class TestFeatureDict(TestCase):
             'wu': False,  # isupper
             'wt': True,   # istitle
             'wd': False,  # isdigit
-            'pw': '<s>',
             'nw': 'gato',
             'nwu': False,
             'nwt': False,
             'nwd': False,
+            'pw': '<s>',
+            'pwu': False,
+            'pwt': False,
+            'pwd': False,
         }
 
         self.assertEqual(feature_dict(sent, 0), fdict)
+
+    def test_feature_dict2(self):
+        sent = 'El Gato come pescado .'.split()
+
+        fdict = {
+            'w': 'el',    # lower
+            'wu': False,  # isupper
+            'wt': True,   # istitle
+            'wd': False,  # isdigit
+            'pw': '<s>',
+            'nw': 'gato',
+            'nwu': False,
+            'nwt': True,
+            'nwd': False,
+            'pwu': False,
+            'pwt': False,
+            'pwd': False,
+        }
+
+        self.assertEqual(feature_dict(sent, 0), fdict)
+
+    def test_feature_dict3(self):
+        sent = 'El Gato come pescado .'.split()
+
+        fdict = {
+            'w': '.',    # lower
+            'wu': False,  # isupper
+            'wt': False,   # istitle
+            'wd': False,  # isdigit
+            'pw': 'pescado',
+            'nw': '</s>',
+            'nwu': False,
+            'nwt': False,
+            'nwd': False,
+            'pwu': False,
+            'pwt': False,
+            'pwd': False,
+        }
+
+        self.assertEqual(feature_dict(sent, 4), fdict)
+
+    def test_feature_dict_n_exception(self):
+        sent = 'El Gato come pescado .'.split()
+        self.assertRaises(IndexError, feature_dict, sent, 1, len(sent))
+        self.assertRaises(IndexError, feature_dict, sent, 1, 0)
+
+    def test_feature_dict_i_exception(self):
+        sent = 'El Gato come pescado .'.split()
+        self.assertRaises(IndexError, feature_dict, sent, len(sent), 3)
